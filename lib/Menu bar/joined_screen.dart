e3375581a22d1994be0e203892bbfe87a15c9733
Muhammad Azeem <m.azeemlaptop@gmail.com>
2024-01-09 23:35:48 +0500

    Final Project

--- lib/Menu bar/joined_screen.dart
+++ lib/Menu bar/joined_screen.dart
@@ -9,77 +9,186 @@ class JoinedScreen extends StatefulWidget {
 
 class _JoinedScreenState extends State<JoinedScreen> {
   final String? userId = userID;
-  bool isLoading = true; // Track initial loading state
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
-        title: Text('Events for User',style: TextStyle(color: Colors.white),),
+        title: Text(
+          'Events for User',
+          style: TextStyle(color: Colors.white),
+        ),
       ),
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance
-            .collection('eventJoined')
-            .where('userID', arrayContains: userId)
-            .snapshots(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            // Show a single small CircularProgressIndicator only for the initial loading state
-            if (isLoading) {
-              isLoading = false; // Set isLoading to false after the first CircularProgressIndicator
-              return Center(
-                child: SizedBox(
-                  height: 20.0,
-                  width: 20.0,
-                  child: CircularProgressIndicator(),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            // Card for "Events You Requested"
+            Card(
+              elevation: 3,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 230, 246, 255),
+                  borderRadius: BorderRadius.circular(15.0),
                 ),
-              );
-            } else {
-              return Container(); // Return an empty container for subsequent loading states
-            }
-          }
-
-          // Extract the list of events
-          var events = snapshot.data!.docs;
-
-          return ListView.builder(
-            itemCount: events.length,
-            itemBuilder: (context, index) {
-              var event = events[index];
-              var eventId = event.id;
-
-              // Use the event ID to fetch details from the 'event' collection
-              return FutureBuilder(
-                future: FirebaseFirestore.instance.collection('event').doc(eventId).get(),
-                builder: (context, eventSnapshot) {
-                  if (!eventSnapshot.hasData) {
-                    // Return an empty container for loading states during event details retrieval
-                    return Container();
-                  }
-
-                  var eventData = eventSnapshot.data!.data();
-                  var eventTitle = eventData!['title'];
-                  var eventUserId = eventData['userID'];
-
-                  // Check if the event's userID does not match the userID in the 'eventJoined' collection
-                  if (eventUserId != userId) {
-                    // Display a card with the event title
-                    return Card(
-                      child: ListTile(
-                        title: Text(eventTitle),
-                        // Add other details if needed
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Events You Requested',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    );
-                  } else {
-                    // Return an empty container if the event should be ignored
-                    return Container();
-                  }
-                },
-              );
-            },
-          );
-        },
+                      SizedBox(height: 8),
+                      StreamBuilder(
+                        stream: FirebaseFirestore.instance
+                            .collection('eventJoined')
+                            .where('userID', arrayContains: userId)
+                            .snapshots(),
+                        builder: (context, snapshot) {
+                          if (!snapshot.hasData) {
+                            return CircularProgressIndicator();
+                          }
+
+                          var events = snapshot.data!.docs;
+
+                          return Container(
+                            height:
+                                265, // Set a fixed height or adjust accordingly
+                            child: ListView.builder(
+                              shrinkWrap: true,
+                              itemCount: events.length,
+                              itemBuilder: (context, index) {
+                                var event = events[index];
+                                var eventId = event.id;
+
+                                return FutureBuilder(
+                                  future: FirebaseFirestore.instance
+                                      .collection('event')
+                                      .doc(eventId)
+                                      .get(),
+                                  builder: (context, eventSnapshot) {
+                                    if (!eventSnapshot.hasData) {
+                                      return Container();
+                                    }
+
+                                    var eventData = eventSnapshot.data!.data();
+                                    var eventTitle = eventData!['title'];
+
+                                    return Card(
+                                      child: Container(
+                                        decoration: BoxDecoration(
+                                          color: Colors.white,
+                                          borderRadius:
+                                              BorderRadius.circular(15.0),
+                                        ),
+                                        child: ListTile(
+                                          title: Text(eventTitle),
+                                          // Add other details if needed
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                );
+                              },
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+
+            // Card for "Confirmed Events"
+            Card(
+              elevation: 3,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 230, 246, 255),
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Confirmed Events',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      StreamBuilder(
+                        stream: FirebaseFirestore.instance
+                            .collection('confirmed_participants')
+                            .where('userID', arrayContains: userId)
+                            .snapshots(),
+                        builder: (context, snapshot) {
+                          if (!snapshot.hasData) {
+                            return CircularProgressIndicator();
+                          }
+
+                          var events = snapshot.data!.docs;
+
+                          return Container(
+                            height:
+                                240, // Set a fixed height or adjust accordingly
+                            child: ListView.builder(
+                              shrinkWrap: true,
+                              itemCount: events.length,
+                              itemBuilder: (context, index) {
+                                var event = events[index];
+                                var eventId = event.id;
+
+                                return FutureBuilder(
+                                  future: FirebaseFirestore.instance
+                                      .collection('event')
+                                      .doc(eventId)
+                                      .get(),
+                                  builder: (context, eventSnapshot) {
+                                    if (!eventSnapshot.hasData) {
+                                      return Container();
+                                    }
+
+                                    var eventData = eventSnapshot.data!.data();
+                                    var eventTitle = eventData!['title'];
+
+                                    return Card(
+                                      child: Container(
+                                        decoration: BoxDecoration(
+                                          color: Colors.white,
+                                          borderRadius:
+                                              BorderRadius.circular(15.0),
+                                        ),
+                                        child: ListTile(
+                                          title: Text(eventTitle),
+                                          // Add other details if needed
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                );
+                              },
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
